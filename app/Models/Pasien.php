c0a0f44202a6291fb77121a0eb33c3a59c69dafb
Annisa Fathilah <fathilah.annisa@gmail.com>
2025-09-27 11:31:07 +0700

    Upload Project Klinik pertama

---
+++ app/Models/Pasien.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pasien extends Model
+{
+    use \Illuminate\Database\Eloquent\Factories\HasFactory;
+}
---
+++ resources/views/pasien_index.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app', ['title' => 'Data Pasien'])
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">Form Pasien</div>
+                    <div class="card-body">
+                        <h3>Data pasien</h3>
+                        <div class="row mb-3 mt-3">
+                            <div class="col-md-6">
+                                <a href="/pasien/create" class="btn btn-primary btn-sm">Tambah Pasien</a>
+                            </div>
+                        </div>
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>NO</th>
+                                    <th>No Pasien</th>
+                                    <th>Nama</th>
+                                    <th>Umur</th>
+                                    <th>Jenis Kelamin</th>
+                                    <th>Alamat</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($pasien as $item)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{ $item->no_pasien }}</td>
+                                        <td>
+                                            {{ $item->nama }}
+                                        </td>
+                                        <td>{{ $item->umur }}</td>
+                                        <td>
+                                         @if($item->foto)
+                                        <img src="{{ asset('storage/'.$item->foto) }}" width="50">
+                                        @endif
+                                        </td>
+                                        <td>{{ $item->jenis_kelamin }}</td>
+                                        <td>{{ $item->alamat }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                        {!! $pasien->links() !!}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
